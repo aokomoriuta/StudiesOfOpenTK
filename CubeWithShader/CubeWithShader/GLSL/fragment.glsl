36844c5192c2f0065156bdd77a6dfd0b93bc1c53
aokomoriuta_lab <internal@enmps.net>
2012-04-25 15:10:41 +0900

    [CubeWithShader] GLSLを3.30に

--- CubeWithShader/CubeWithShader/GLSL/fragment.glsl
+++ CubeWithShader/CubeWithShader/GLSL/fragment.glsl
@@ -1,9 +1,21 @@
-//! input: diffusing color
-varying vec4 diffuseColor;
+#version 330
+
+//! input: normal vector of vertex
+in vec3 vertexNormal;
+
+//! input: direction of light
+uniform vec3 lightDirection;
+
+//! output: color
+out vec4 outColor;
+
 
 //! entry point
 void main()
 {
-	// set color with diffusing color
-	gl_FragColor = diffuseColor;
+	//! calulate diffusing color by normal vector of vertex and direction of light
+	outColor = vec4(max(0.0,dot(vertexNormal,-lightDirection)));
+	
+	//! full opacity
+	outColor.a = 1.0;
 }
--- CubeWithShader/CubeWithShader/GLSL/vertex.glsl
+++ CubeWithShader/CubeWithShader/GLSL/vertex.glsl
@@ -1,8 +1,11 @@
+#version 330
+
 //! input: position of vertex
-attribute vec3 position;
+in vec3 position;
 
 //! input: normal vector of vertex
-attribute vec3 normal;
+in vec3 normal;
+
 
 //! input: view (camera)
 uniform mat4 view;
@@ -10,11 +13,10 @@ uniform mat4 view;
 //! input: projection matrix
 uniform mat4 projection;
 
-//! input: direction of light
-uniform vec3 lightDirection;
 
-//! output: diffusing color
-varying vec4 diffuseColor;
+//! output: normal vector after vertex shader
+out vec3 vertexNormal;
+
 
 //! entry point
 void main()
@@ -22,9 +24,6 @@ void main()
 	//! calculate position by view, projection and position of vertex
 	gl_Position = projection*view * vec4(position, 1.0);
 
-	//! calulate diffusing color by normal vector of vertex and direction of light
-	diffuseColor = vec4(max(0,dot(normal,-lightDirection)));
-	
-	//! full opacity
-	diffuseColor.a = 1.0;
+	//! set normal vector same as input
+	vertexNormal = normal;
 }
