bbf425c7eb60b132cf7548b3a321e2da205eed24
aokomoriuta_lab <internal@enmps.net>
2012-05-22 15:28:19 +0900

    PhongShading追加

---
+++ PhongShading/PhongShading/Cube.cs
@@ -0,0 +1,44 @@
+﻿using OpenTK;
+namespace LWisteria.StudiesOfOpenTK.PhongShading
+{
+	/// <summary>
+	/// 立方体
+	/// </summary>
+	struct Cube
+	{
+		/// <summary>
+		/// Cubeのデータサイズ
+		/// </summary>
+		public static readonly int SizeInByte = System.Runtime.InteropServices.Marshal.SizeOf(default(Cube));
+
+		/// <summary>
+		/// 立方体の中心座標
+		/// </summary>
+		readonly Vector3 position;
+
+		/// <summary>
+		/// 立方体の1辺の長さ
+		/// </summary>
+		readonly float size;
+
+		/// <summary>
+		/// 立方体の表示色
+		/// </summary>
+		readonly Vector3 color;
+
+		/// <summary>
+		/// 立方体を作成する
+		/// </summary>
+		/// <param name="_position">中心座標</param>
+		/// <param name="_size">1辺の長さ</param>
+		/// <param name="_color">表示色</param>
+		public Cube(Vector3 _position, float _size, Vector3 _color)
+		{
+			// 各パラメーターを設定
+			this.position = _position;
+			this.size = _size;
+			this.color = _color;
+		}
+		
+	}
+}
---
+++ PhongShading/PhongShading/PhongShadingMain.cs
@@ -0,0 +1,24 @@
+﻿namespace LWisteria.StudiesOfOpenTK.PhongShading
+{
+	/// <summary>
+	/// メインクラス
+	/// </summary>
+	partial class PhongShadingMain : System.Windows.Application
+	{
+		/// <summary>
+		/// 最上位ウインドウ
+		/// </summary>
+		MainWindow mainWindow;
+
+		/// <summary>
+		/// メインクラスを開始
+		/// </summary>
+		public PhongShadingMain()
+		{
+			// 最上位ウインドウを作成して設定
+			this.mainWindow = new MainWindow();
+			base.MainWindow = this.mainWindow;
+			base.MainWindow.Show();
+		}
+	}
+}
